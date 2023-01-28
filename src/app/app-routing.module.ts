f1c5300fc6430cd6f7314be1e7daffc4c5732f31
roosafeed <roosafeedkoyakp811@gmail.com>
2023-01-28 18:33:05 +0530

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
-    path: "home",
+    path: "",
     component: HomeComponent
   },
   {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
     path: "**",
-    redirectTo: "/home",
+    redirectTo: "/",
     pathMatch: "full"
   }
 ];
