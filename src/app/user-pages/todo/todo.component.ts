0ac5aa7db7c22b4c947d7e84a36cdcb7395d50ce
roosafeed <roosafeedkoyakp811@gmail.com>
2023-02-08 01:18:04 +0530

    doc change

--- src/app/user-pages/todo/todo.component.ts
+++ src/app/user-pages/todo/todo.component.ts
@@ -7,17 +7,7 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX - The Rise of Skywalker',
-  ];
+  movies = [];
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
